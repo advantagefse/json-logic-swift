51a6e64e4ae2799538ab19bf14f3bc6fb6f5ea3b
Dino Srdoč <dino.srdoc@bornfight.com>
2019-07-22 10:44:26 +0200

    add more tests

--- Tests/jsonlogicTests/NumericalOperations/RoundingTests.swift
+++ Tests/jsonlogicTests/NumericalOperations/RoundingTests.swift
@@ -40,4 +40,33 @@ class RoundingTests: XCTestCase {
         XCTAssertEqual(0.0, try applyRule(rule, to: nil))
     }
 
+    func testRounding_oneDecimal() {
+        var rule =
+        """
+        { "rnd": [1.234, 1] }
+        """
+        
+        XCTAssertEqual(1.2, try applyRule(rule, to: nil))
+        
+        rule =
+        """
+        { "rnd": [1.789, 1] }
+        """
+        
+        XCTAssertEqual(1.8, try applyRule(rule, to: nil))
+        
+        rule =
+        """
+        { "rnd": [-0.7, 1] }
+        """
+        
+        XCTAssertEqual(-0.7, try applyRule(rule, to: nil))
+        
+        rule =
+        """
+        { "rnd": [-0.445, 1] }
+        """
+        
+        XCTAssertEqual(-0.4, try applyRule(rule, to: nil))
+    }
 }
