4624b6e2893bb40e8f7ce532be460419df70e388
Dino Srdoč <dino.srdoc@bornfight.com>
2019-07-22 10:42:59 +0200

    dont expect integers as output of round

--- Tests/jsonlogicTests/NumericalOperations/RoundingTests.swift
+++ Tests/jsonlogicTests/NumericalOperations/RoundingTests.swift
@@ -16,28 +16,28 @@ class RoundingTests: XCTestCase {
         { "rnd": [1.234, 0] }
         """
         
-        XCTAssertEqual(1, try applyRule(rule, to: nil))
+        XCTAssertEqual(1.0, try applyRule(rule, to: nil))
         
         rule =
         """
         { "rnd": [1.789, 0] }
         """
         
-        XCTAssertEqual(2, try applyRule(rule, to: nil))
+        XCTAssertEqual(2.0, try applyRule(rule, to: nil))
         
         rule =
         """
         { "rnd": [-0.7, 0] }
         """
         
-        XCTAssertEqual(-1, try applyRule(rule, to: nil))
+        XCTAssertEqual(-1.0, try applyRule(rule, to: nil))
         
         rule =
         """
         { "rnd": [-0.4, 0] }
         """
         
-        XCTAssertEqual(0, try applyRule(rule, to: nil))
+        XCTAssertEqual(0.0, try applyRule(rule, to: nil))
     }
 
 }
