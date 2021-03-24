60c99362255370cf3adb609cf72b5123d196dc17
Christos Koninis <christos.koninis@gmail.com>
2021-03-24 23:23:54 +0200

    Declaring supported platforms in manifest

--- Package.swift
+++ Package.swift
@@ -5,6 +5,9 @@ import PackageDescription
 
 let package = Package(
     name: "jsonlogic",
+    platforms: [
+        .macOS(.v10_10), .iOS(.v8), .tvOS(.v9), .watchOS(.v2)
+    ],
     products: [
         .library(
             name: "jsonlogic",
